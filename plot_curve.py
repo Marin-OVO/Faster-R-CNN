8381ef35eae41ba27a64c59f13a42b193e513146
Marin-OVO <1922590765@qq.com>
2026-01-08 21:08:32 +0800

    add:README.md

--- plot_curve.py
+++ plot_curve.py
@@ -22,7 +22,7 @@ def plot_loss_and_lr(train_loss, learning_rate):
         handles2, labels2 = ax2.get_legend_handles_labels()
         plt.legend(handles1 + handles2, labels1 + labels2, loc='upper right')
 
-        fig.subplots_adjust(right=0.8)  # 防止出现保存图片显示不全的情况
+        fig.subplots_adjust(right=0.8)
         fig.savefig('./loss_and_lr{}.png'.format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
         plt.close()
         print("successful save loss curve! ")
